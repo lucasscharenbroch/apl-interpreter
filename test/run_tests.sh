ad94c938540e0385740668574a7aee11f65c4237
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2023-11-28 18:51:07 -0600

    Add command lines args to tests/run_tests.sh

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -20,7 +20,15 @@ for file in ${test_files[*]}; do
     ai_out=$(../bin/ai <$file)
     dyalog_out=$( (dyalog <<<"${box_prefix}$(cat $file)" 2>/dev/null | tail +2 ) )
 
-    # diff -Z <(echo "$ai_out") <(echo "$dyalog_out")
-    diff -w <(echo "$ai_out") <(echo "$dyalog_out")
-    # vimdiff <(echo "$ai_out") <(echo "$dyalog_out")
+    arg=${1:--w}
+
+    if [ $arg = "-v" ]; then
+        vimdiff <(echo "$ai_out") <(echo "$dyalog_out")
+    elif [ $arg = "-Z" ]; then
+        diff -Z <(echo "$ai_out") <(echo "$dyalog_out")
+    elif [ $arg = "-w" ]; then
+        diff -w <(echo "$ai_out") <(echo "$dyalog_out")
+    else
+        echo "invalid argument: $arg"
+    fi;
 done
