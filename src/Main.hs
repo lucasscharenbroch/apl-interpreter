9485db1e321e34997b23058e330d38b8c3a09355
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2023-12-24 14:27:02 -0600

    Implement verbosity

--- src/Main.hs
+++ src/Main.hs
@@ -11,7 +11,14 @@ import System.Console.GetOpt
 import System.Environment (getArgs)
 import Data.List (intersperse)
 import System.Exit
-import Data.IORef
+
+{- Global State -}
+
+data GlobalState = GlobalState {
+       verbosity :: Bool -- display trees before evaluation?
+     }
+
+defaultGlobalState = GlobalState False
 
 {- Option Parsing -}
 
@@ -21,7 +28,7 @@ data CliOption = VerboseFlag | HelpFlag
 
 optDescriptions :: [OptDescr CliOption]
 optDescriptions = [
-        Option "v" ["verbose"] (NoArg VerboseFlag) "print array trees before evaluation",
+        Option "v" ["verbose"] (NoArg VerboseFlag) "print array/function/operator trees before evaluation",
         Option "h" ["help"] (NoArg HelpFlag) "print help information"
     ]
 
@@ -35,21 +42,22 @@ parseArgv = do
         (opts, _, errs) -> ioError . userError $ errStr
             where errStr = "\nInvalid argument(s): " ++ concat errs ++ usageInfo usageHeader optDescriptions
 
-handleArgv :: [CliOption] -> IO ()
-handleArgv [] = return ()
+handleArgv :: [CliOption] -> IO GlobalState
+handleArgv [] = return defaultGlobalState
 handleArgv (HelpFlag:opts) = do
     putStr $ usageInfo usageHeader optDescriptions
     handleArgv opts
     exitWith ExitSuccess
 handleArgv (VerboseFlag:opts) = do
-    -- TODO ...
-    handleArgv opts
+    handleArgv opts >>= \x -> return $ x { verbosity = True }
 
 {- Constants -}
 
 defaultIdMap = mapInsert "⎕IO" (IdArr . arrFromList $ [ScalarNum 1]) emptyIdMap
 
 {- Helpers -}
+
+lift2 = lift . lift
 
 evalArrTree' :: IdMap -> ArrTreeNode -> IO (IdMap, Array)
 evalArrTree' idm atn = (\(r, i) -> (i, r)) <$> runStateT (evalArrTree atn) idm
@@ -60,43 +68,50 @@ evalFnTree' idm ftn = (\(r, i) -> (i, expectFunc r)) <$> runStateT (evalFnTree f
 evalOpTree' :: IdMap -> OpTreeNode -> IO (IdMap, Operator)
 evalOpTree' idm otn = (\(r, i) -> (i, r)) <$> runStateT (evalOpTree otn) idm
 
-handleRes :: IdMap -> ExprResult -> InputT IO IdMap
+handleRes :: IdMap -> ExprResult -> ReaderT GlobalState (InputT IO) IdMap
 handleRes idMap x = case x of
-    (ResAtn a True) -> do (i, a') <- lift $ evalArrTree' idMap a
-                          outputStrLn . show $ a'
+    (ResAtn a True) -> do (i, a') <- lift2 $ evalArrTree' idMap a
+                          showIfVerbose a
+                          lift $ outputStrLn . show $ a'
                           return i
-    (ResAtn a False) -> do (i, _) <- lift $ evalArrTree' idMap a
+    (ResAtn a False) -> do (i, _) <- lift2 $ evalArrTree' idMap a
+                           showIfVerbose a
                            return i
-    (ResFtn f True) -> do (i, f') <- lift $ evalFnTree' idMap f
-                          outputStrLn . show $ f'
+    (ResFtn f True) -> do (i, f') <- lift2 $ evalFnTree' idMap f
+                          showIfVerbose f
+                          lift $ outputStrLn . show $ f'
                           return i
-    (ResFtn f False) -> do (i, _) <- lift $ evalFnTree' idMap f
+    (ResFtn f False) -> do (i, _) <- lift2 $ evalFnTree' idMap f
+                           showIfVerbose f
                            return i
-    (ResOp o True) -> do (i, o') <- lift $ evalOpTree' idMap o
-                         outputStrLn . show $ o'
+    (ResOp o True) -> do (i, o') <- lift2 $ evalOpTree' idMap o
+                         showIfVerbose o
+                         lift $ outputStrLn . show $ o'
                          return i
-    (ResOp o False) -> do (i, o') <- lift $ evalOpTree' idMap o
+    (ResOp o False) -> do (i, o') <- lift2 $ evalOpTree' idMap o
+                          showIfVerbose o
                           return i
     (ResNull) -> return idMap
+    where showIfVerbose :: (Show a) => a -> ReaderT GlobalState (InputT IO) ()
+          showIfVerbose x = ask >>= \state -> case verbosity state of
+              True -> lift $ outputStrLn . show $ x
+              False -> return ()
 
-execStatement :: IdMap -> [Token] -> InputT IO IdMap
+execStatement :: IdMap -> [Token] -> ReaderT GlobalState (InputT IO) IdMap
 execStatement idm [] = return idm
 execStatement idm ts = case evalMatchFn idm ts parseExpr of
-    Nothing -> do outputStrLn "parse error" -- TODO syntax error
+    Nothing -> do lift $ outputStrLn "parse error" -- TODO syntax error
                   return idm
     Just (res, ts') -> do idm' <- handleRes idm res
                           execStatement idm' ts'
 
-mainloop :: IdMap -> [Token] -> InputT IO ()
+mainloop :: IdMap -> [Token] -> ReaderT GlobalState (InputT IO) ()
 mainloop idMap carriedToks = do
-    isInterractive <- haveTerminalUI
-    input <- getInputLine $ if isInterractive then ps else ""
+    isInterractive <- lift $ haveTerminalUI
+    input <- lift $ getInputLine $ if isInterractive then ps else ""
     case input of
         Nothing -> return ()
-        Just s -> do if False
-                     then outputStrLn . show . tokenize $ s
-                     else return ()
-                     let toks = carriedToks ++ tokenize s
+        Just s -> do let toks = carriedToks ++ tokenize s
                      let nestingLevel' = countBracketNesting toks
                      let (mIdMap', carriedToks')  = if nestingLevel' <= 0 then (execStatement idMap toks, [])
                                                     else (return idMap, toks ++ [ChTok '\n'])
@@ -108,6 +123,6 @@ mainloop idMap carriedToks = do
 
 main :: IO ()
 main = do
-    parseArgv >>= handleArgv
-    runInputT settings (mainloop defaultIdMap [])
+    gState <- parseArgv >>= handleArgv
+    runInputT settings $ (flip runReaderT) gState $ (mainloop defaultIdMap [])
     where settings = setComplete completeGlyph defaultSettings
